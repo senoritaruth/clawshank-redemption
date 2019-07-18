2f6afa6aa84c34c319300ae0f5acfee94dbab330
Ruth Cisneros <senoritaruth@gmail.com>
2019-07-17 23:49:10 -0600

    created the milestone document

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # clawshank-redepmtion
-Personal website project
+Personal website project: from the moment Killer Cally Calabaza climbed into our homes and hearts, she changed her new brother's life forever. This is the story of Greyson and Cally.
---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Personal Website Project: Clawshank Redemption</title>
+	</head>
+<body>
+	<div>
+<h1>Personal Website Project: Clawshank Redemption</h1>
+	</div>
+	<div>
+<h2>Purpose, Audience, Goal</h2>
+	<ol>
+		<li>Purpose:</li>
+		The purpose of this website is to practice the art of digital storytelling while maintaining best practices in creating a beautiful, well-deployed website.
+		<li>Audience:</li>
+		This website is geared toward kids and cat-ladies. As it turns out, I am related to both and I want to share the adventures of my cats with them, whether they are nearby or far away.
+		<li>Goal:</li>
+		I would like to share my cats' personal stories while also showcasing my technical and writing skills.
+	</ol>
+	</div>
+	<div>
+<h2>Persona</h2>
+
+	</div>
+</body>
