fd857b351c86d8e95a79f54f8c7578e20f6a0e60
Ruth Cisneros <senoritaruth@gmail.com>
2019-07-31 23:54:25 -0600

    created milestone 2 doc, including purpose of the website and the different sections.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+			<title>Content Strategy</title>
+	</head>
+	<body>
+		<h2>Welcome</h2>
+		<p>A brief introduction of the cats, and a teaser as to why this site is called Clawshank Redemption. I will include a picture of both of them. </p>
+		<br>
+		<h2>Pictures</h2>
+		<p>The main feature of the website will be photos of the cats. I will be putting together a gallery of their best adventures from their first year together. These will include embeds from Instagram as well as never-before published shots taken around the house. I will also illustrate how the great escape happened.</p>
+		<br>
+		<h2>The rest of the story</h2>
+		<p>Why do we call the cats Clawshank Redemption? This is the part of the webpage where we find out how Cally has changed her older brother from a retiring gentleman to a spry guy with a second lease on life.</p>
+		<br>
+		<h2>Contact form</h2>
+		<p></p>
+	</body>
