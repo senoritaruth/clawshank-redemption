a9bc9db856305dba295be4486afd2a4eb6ab101e
Ruth Cisneros <senoritaruth@gmail.com>
2019-08-09 08:03:53 -0600

    first section content

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Clawshank Redemption: The Saga of Greyson & Cally</title>
+	</head>
+	<body>
+	<h1>Welcome to our little home</h1>
+		<p>Last May, my foster mom found me on a movie set with my tiny little brother and sisters. For whatever reason, our mom didn't return after twelve hours and my foster mom decided to take us home instead of leaving us on the sand dune overnight. She became our foster mom and we lived at her house for the next few weeks. Shortly after she found us, we took us on a field trip to he son's graduation party. We had to stay in the back room, in a drawer filled with soft blankets. That's where this lady picked me up and looked me in my tiny, unfocused eyeballs. Her face lit up, and when she went to snuggle me, I licked her lips. A few weeks after that, she picked me up from the pound, and I went home with my new family. </p>
+	</body>
