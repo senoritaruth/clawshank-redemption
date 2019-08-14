1cf9573b3ae3f5552f8665f462ffd67244673ce3
Ruth Cisneros <senoritaruth@gmail.com>
2019-08-14 08:56:09 -0600

    added div containers

--- public_html/index.php
+++ public_html/index.php
@@ -18,13 +18,6 @@
 		<script src='https://www.google.com/recaptcha/api.js'></script>
 		<title>Clawshank Redemption: The Saga of Greyson & Cally</title>
 	</head>
-	<!-- Image and text -->
-	<nav class="navbar navbar-light bg-light">
-		<a class="navbar-brand" href="#">
-			<img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-			Bootstrap
-		</a>
-	</nav>
 	<body>
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<a class="navbar-brand" href="#">Navbar</a>
@@ -46,6 +39,7 @@
 			</div>
 		<p>Last May, my foster mom found me on a movie set with my tiny little brother and sisters. For whatever reason, our mom didn't return after twelve hours and my foster mom decided to take us home instead of leaving us on the sand dune overnight. She became our foster mom and we lived at her house for the next few weeks. Shortly after she found us, we took us on a field trip to he son's graduation party. We had to stay in the back room, in a drawer filled with soft blankets. That's where this lady picked me up and looked me in my tiny, unfocused eyeballs. Her face lit up, and when she went to snuggle me, I licked her lips. A few weeks after that, she picked me up from the pound, and I went home with my new family. </p>
 		</div>
+		<div class = container>
 		<div class = "GreyHeader">
 	<h2>Check out some of our favorite memories</h2>
 		<div class="bd-example">
@@ -88,31 +82,22 @@
 				</a>
 			</div>
 		</div>
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<a class="navbar-brand" href="#">Navbar</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-				<div class="navbar-nav">
-					<a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-					<a class="nav-item nav-link" href="#">Features</a>
-					<a class="nav-item nav-link" href="#">Pricing</a>
-					<a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-				</div>
-			</div>
-		</nav>
+		</div>
 		<div class="container">
 		</div>
 			<div id = OrangeHeader>
 			<h2>...The rest of the story</h2>
 			<p>I'm Greyson. Since 2013, I've enjoyed the near-total attention of my parents. I love licking ice cubes and staring out the window. Before last summer, I could lay anywhere without the risk of being pounced on. Life was good, quiet. I had given up hunting cockroaches years before, a young cat's game. Then suddenly, a streak of orange appeared. She was so tiny, it was hard to keep track of her. She smelled familiar enough, but I had never seen her before. What had happened to the quiet life of a retiring bachelor cat? Suddenly, I was never alone. I had a tiny maw chewing on my head at all times of day and night. She wanted to fight, but I knew I could crush her easily with my giant grey paw. But I didn't want to. Somehow, the orange streak grew on me and now we play and nap together. She's shown me some bad habits and I've shown her the lay of the house. One day, we figured out how to escape. It was the greatest adventure I'd had in a long while.</p>
 		</div>
+			<div class = container>
 			<div class = "GreyHeader">
 			<h2>You can follow our adventures too!</h2>
 
 				<p>social media links</p>
+			</div>
+			</div>
 
+			<div class = container>
 			<h2>Do you have a treat? Contact us!</h2>
 				<form id="clawshank-redemption" action="php/mailer.php" method="post">
 					<div class="form-group">
@@ -169,7 +154,7 @@
 						<div id="output-area"></div>
 					</div>
 				</div>
-
+				</div>
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
