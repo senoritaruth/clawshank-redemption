d6e3bdbef5763c29e4bdbc68a3647ef77b6b7c1d
Ruth Cisneros <senoritaruth@gmail.com>
2019-08-14 10:29:59 -0600

    changed the javascript validate file directory pointer in the head info

--- public_html/index.php
+++ public_html/index.php
@@ -12,7 +12,7 @@
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
 
 		<!-- Your JavaScript Form Validator -->
-		<script src="js/form-validate.js"></script>
+		<script src="JavaScript/js-validator.js"></script>
 
 		<!-- Google reCAPTCHA -->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
