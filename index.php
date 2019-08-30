ec473d2dbbb6ded62e5f2a7783415ad51bfc2caf
Ruth Cisneros <senoritaruth@gmail.com>
2019-08-30 10:32:55 -0600

    Working to fix the favicon disaster. :Revert "no favicon"
    This reverts commit af7f54c0d4055a2f33dd9860537c5b39dd194cf7.

--- index.php
+++ index.php
@@ -20,6 +20,13 @@
 		<script src="https://kit.fontawesome.com/258ed8e229.js"></script>
 		<!-- JavaScript Form Validator -->
 		<script src="../JavaScript/js-validator.js"></script>
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="manifest" href="/site.webmanifest">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="theme-color" content="#ffffff">
 		<title>Clawshank Redemption: The Saga of Greyson & Cally</title>
 	</head>
 	<body>
